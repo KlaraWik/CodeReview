725832c1d2ff776d8cb35a2ed7d2c45948fd9ea0
Rebecca Liss <liss@MacBook-Pro.local>
2021-12-01 16:20:16 +0100

    it works now

--- codeReview/src/codeReview.java
+++ codeReview/src/codeReview.java
@@ -5,25 +5,26 @@ public class codeReview{
 	
 	public static void main(String[] args) throws FileNotFoundException{
 		
-		System.out.println("search for word");
-		
-		Scanner scan = new Scanner(System.in); 
-		String s = scan.nextLine();
+		//System.out.println("search for word");
+		String word = args[0];
+		String searchFile = args[1];
+		//Scanner scan = new Scanner(System.in);
+		//String s = scan.nextLine();
 		
 		// få in textfil
-		File file = new File("nilsholg.txt");
+		//File file = new File("nilsholg.txt");
+		File file = new File(searchFile);
 
 		//InputStream stream = StringSearcher.class.getResourceAsStream("file.txt");
 		Scanner text = new Scanner(file); // vilken fil som ska letas
 		
 		while(text.hasNextLine()) {
-			String line = scan.nextLine();
-			
-			if(line.contains(s)) {
-				 System.out.println(line);
+			String line = text.nextLine();
+
+			if (line.contains(word)) {
+				System.out.println(line);
 			}
-		}  
-		scan.close();
+		}
 		text.close();
     }
 }
