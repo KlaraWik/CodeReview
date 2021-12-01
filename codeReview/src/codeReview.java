2a7c2c929b63767fad4ce83af5daabeaa97ae16f
KlaraWik <61154626+KlaraWik@users.noreply.github.com>
2021-12-01 11:53:13 +0100

    Add files via upload

--- codeReview/src/codeReview.java
+++ codeReview/src/codeReview.java
@@ -1,10 +1,29 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class codeReview{
 	
-	public static void main(String[] args)
-    {
-        System.out.println("hejsan hoppsan");
-       
+	public static void main(String[] args) throws FileNotFoundException{
+		
+		System.out.println("search for word");
+		
+		Scanner scan = new Scanner(System.in); 
+		String s = scan.nextLine();
+		
+		// få in textfil
+		File file = new File("nilsholg.txt");
+
+		//InputStream stream = StringSearcher.class.getResourceAsStream("file.txt");
+		Scanner text = new Scanner(file); // vilken fil som ska letas
+		
+		while(text.hasNextLine()) {
+			String line = scan.nextLine();
+			
+			if(line.contains(s)) {
+				 System.out.println(line);
+			}
+		}  
+		scan.close();
+		text.close();
     }
 }
