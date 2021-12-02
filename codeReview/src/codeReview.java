1cb03ca699c141595b26ee0b704f355286248e3e
Rebecca Liss <liss@MacBook-Pro.local>
2021-12-02 14:15:37 +0100

    new changes

--- codeReview/src/codeReview.java
+++ codeReview/src/codeReview.java
@@ -4,19 +4,12 @@ import java.util.Scanner;
 public class codeReview{
 	
 	public static void main(String[] args) throws FileNotFoundException{
-		
-		//System.out.println("search for word");
 		String word = args[0];
 		String searchFile = args[1];
-		//Scanner scan = new Scanner(System.in);
-		//String s = scan.nextLine();
-		
-		// få in textfil
-		//File file = new File("nilsholg.txt");
+
 		File file = new File(searchFile);
 
-		//InputStream stream = StringSearcher.class.getResourceAsStream("file.txt");
-		Scanner text = new Scanner(file); // vilken fil som ska letas
+		Scanner text = new Scanner(file); //vilken fil som ska letas
 		
 		while(text.hasNextLine()) {
 			String line = text.nextLine();
